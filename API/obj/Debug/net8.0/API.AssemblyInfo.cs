6b4d914f6cb1c87a3d860a8ef24bc6c4a85bfcad
Gabriel Onohara <gabakio11@gmail.com>
2024-03-30 12:52:27 -0300

    Seeding data and update to async methods in db

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2935a291d099aba44d27758610d8e7374158aafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c650ef9a663110e43498d6d42b66be418d3c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
