dcfe97f16be6d8e3f7bed37739764e5e29ca6f6e
Gabriel Onohara <gabakio11@gmail.com>
2024-04-02 16:48:11 -0300

    using activity interface with ts

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0841bad051aafe0a513d2dae4683fb1b00c95a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39224052d1d5f74d2c5ef91d25000c56303a6b85")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
