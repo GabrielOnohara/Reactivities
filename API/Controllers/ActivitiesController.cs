d8696b485f7f4627e282deb4d15fb1006372ddb6
Gabriel Onohara <gabakio11@gmail.com>
2024-04-01 21:59:25 -0300

    apply in super class

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,16 +8,11 @@ namespace API.Controllers
 {
     public class ActivitiesController:BasicApiController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            this._mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
