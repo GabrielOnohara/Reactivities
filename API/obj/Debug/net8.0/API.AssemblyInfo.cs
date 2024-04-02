a6faac2a2995533ccf45f2ebfc8b47d1b7539aab
Gabriel Onohara <gabakio11@gmail.com>
2024-04-01 22:18:28 -0300

    add post  create command

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8696b485f7f4627e282deb4d15fb1006372ddb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfee966d83d410312382471f632381937b7e34c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
