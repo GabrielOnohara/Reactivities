5eaee66dfb7c25c8f5ef311edc3f2a2e1c0ded08
Gabriel Onohara <gabakio11@gmail.com>
2024-04-02 17:06:28 -0300

    creating a navbar

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39224052d1d5f74d2c5ef91d25000c56303a6b85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfe97f16be6d8e3f7bed37739764e5e29ca6f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
