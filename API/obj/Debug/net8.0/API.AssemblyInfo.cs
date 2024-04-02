6a4015849a40dccd56f1116a6d753bfcc5578448
Gabriel Onohara <gabakio11@gmail.com>
2024-04-01 23:08:04 -0300

    Creating delete method

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6faac2a2995533ccf45f2ebfc8b47d1b7539aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2c079c96b8681913c2c719cec692442b1cfe72")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -2,35 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
         public class Command : IRequest
         {
-            public Activity Activity {get; set;}
+            public Guid Id {get; set;}
         }
 
-        public class Handler : IRequestHandler<Command> 
+        public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            private readonly IMapper _mapper;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
                 _context = context;
-                _mapper = mapper;
             }
 
             public async Task Handle(Command request, CancellationToken cancellationToken)
             {
-                Activity activity = await _context.Activity.FindAsync(request.Activity.Id);
-                _mapper.Map(request.Activity, activity);
+                Activity activity = await _context.Activity.FindAsync(request.Id);
+                _context.Remove(activity);
                 await _context.SaveChangesAsync();
             }
         }
