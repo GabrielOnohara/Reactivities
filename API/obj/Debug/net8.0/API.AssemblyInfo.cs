82cd6d32cc1d0d3a732c4b7ed3446d2905c79138
Gabriel Onohara <gabakio11@gmail.com>
2024-04-08 11:52:21 -0300

    refactoring initial stucture

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec12c66e59ade27db051efb36ef8bf59271d417d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53389341da3ef588e2f64bebb6799d37175c1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
