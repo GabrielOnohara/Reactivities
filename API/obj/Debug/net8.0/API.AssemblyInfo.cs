72787d5b432f8668be72f35e70122bb56d2198c1
Gabriel Onohara <gabakio11@gmail.com>
2024-04-08 12:53:50 -0300

    cleaing and solving specific bugs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e246a5241fb3ca98c796e07700226039fd6350fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e30b588379121f5f587352ef10d8ea4e84a44cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { v4 as uuid } from "uuid";
@@ -106,7 +106,8 @@ const ActivityForm = () => {
           onClick={() => handleSubmit}
         ></Button>
         <Button
-          onClick={() => {}}
+          as={Link}
+          to="/activities"
           floated="right"
           type="button"
           content="Cancel"
