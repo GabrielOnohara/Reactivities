43779dd2e73f1ef9c22d4da35e121edab9b8b938
Gabriel Onohara <gabakio11@gmail.com>
2024-04-23 17:42:39 -0300

    removing  unused vars

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933f5d36dcb72636a068b03ffed4199b6ed29494")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca92a8e00648af90259197840cec95f9cd8f36ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
