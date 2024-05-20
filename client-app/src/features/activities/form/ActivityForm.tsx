65d9635d259ec72153b899fea3c6148f705992db
Gabriel Onohara <gabakio11@gmail.com>
2024-05-20 16:08:55 -0300

    add date validator

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -40,7 +40,7 @@ const ActivityForm = () => {
     title: Yup.string().required("The activity title is required"),
     description: Yup.string().required("The activity description is required"),
     category: Yup.string().required(),
-    date: Yup.string().required(),
+    date: Yup.string().required("Date is required").nullable(),
     venue: Yup.string().required(),
     city: Yup.string().required(),
   });
