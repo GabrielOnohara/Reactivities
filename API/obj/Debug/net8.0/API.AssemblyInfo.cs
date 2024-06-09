de4a5fbcbc0a867d19069392f3f2122f9ee95f2c
Gabriel Onohara <gabakio11@gmail.com>
2024-06-09 16:01:34 -0300

    creating login logic

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f8ec099707fbddb9b80d93ea5b082d4263e173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9835a4804a18bc5de090ca70f0422cf2486a158")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
