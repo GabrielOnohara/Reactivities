e625ac391da9a4a50a5fb19e77ef98813380932e
Gabriel Onohara <gabakio11@gmail.com>
2024-06-09 17:21:42 -0300

    creating register validation

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37b3cd96c4e38e6cf60fd277306d02ca25c0789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814f5e67f7260c936aa27639ca018e372f240dd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
