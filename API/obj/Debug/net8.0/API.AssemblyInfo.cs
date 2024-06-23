5b09b84a80ef6879594651b372a62fe457a405d8
Gabriel Onohara <gabakio11@gmail.com>
2024-06-23 16:43:18 -0300

    update atttendance

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e118458c4a3f968c09a54912ba26b42f46a1ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa26c70bf02e3a14d46de50b3705a45c19b6d0b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
