3e118458c4a3f968c09a54912ba26b42f46a1ada
Gabriel Onohara <gabakio11@gmail.com>
2024-06-23 09:52:15 -0300

    initializing infra csproj

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc60d09c634925b77e204741df529d90eca3b73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0654c60731332c7d3839a77c66069dfbf9e9bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
