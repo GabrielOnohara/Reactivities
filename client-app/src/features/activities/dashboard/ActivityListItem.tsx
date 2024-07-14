e4ebfa1df53b168a39f40eeccd89d89f5a60a7e4
Gabriel Onohara <gabakio11@gmail.com>
2024-07-14 13:09:15 -0300

    add host support

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
@@ -19,16 +19,33 @@ const ActivityListItem = ({ activity }: Props) => {
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted By Bob</Item.Description>
+              <Item.Description>
+                Hosted By {activity.host?.displayName}
+              </Item.Description>
+              {activity.isHost && (
+                <Item.Description>
+                  <Label basic color="orange">
+                    You are hosting this activity
+                  </Label>
+                </Item.Description>
+              )}
+              {activity.isGoing && !activity.isHost && (
+                <Item.Description>
+                  <Label basic color="green">
+                    You are going to this activity
+                  </Label>
+                </Item.Description>
+              )}
             </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
       <Segment>
-        <span>
-          <Icon name="clock" /> {format(activity.date!, "dd MMM yyyy h:mm aa")}
-          <Icon name="marker" /> {activity.venue}
-        </span>
+        <Icon name="clock" color="teal" />{" "}
+        {format(activity.date!, "dd MMM yyyy h:mm aa")}
+      </Segment>
+      <Segment>
+        <Icon name="marker" color="teal" /> {activity.venue}
       </Segment>
       <Segment secondary>
         <ActivityListItemAttendee attendees={activity.attendees || []} />
